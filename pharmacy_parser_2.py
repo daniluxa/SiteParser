5b0d3d74874ca85d6162b20b56ce5582a4e7cd16
Daniluxa <shumkin.daniil@mail.ru>
2024-09-01 18:38:15 +0300

    Поправил url

--- pharmacy_parser_2.py
+++ pharmacy_parser_2.py
@@ -12,44 +12,29 @@ from selenium.webdriver.chrome.options import Options
 pharm_name = []
 pharm_price = []
 
-url = 'https://ozerki.ru/sankt-peterburg/catalog/'
+url = 'https://ozerki.ru/sankt-peterburg/catalog'
 availability_of_the_next_page = True
 
-catalog_name = ['lekarstvennye-i-profilakticheskie-sredstva','sredstva-dlya-krasoty-i-gigieny','tovary-dlya-materi-i-rebenka','prodkuty-pitaniya',
-                'meditsinskaya-tekhnika-i-izdeliya-sredstva-ukhoda','optika','-dermakosmetika']
-
-podcatolog_name_1 = ['lekarstva-ot-prostudy-i-grippa', 'preparaty-dlya-pishchevaritelnogo-trakta','preparaty-dlya-serdechno-sosudistoj-sistemy', 
+catalog_name = ['lekarstva-ot-prostudy-i-grippa', 'preparaty-dlya-pishchevaritelnogo-trakta','preparaty-dlya-serdechno-sosudistoj-sistemy', 
                 'preparaty-pri-boleznyah-nervnoj-sistemy','planirovanie_semi', 'fitopreparaty','mama_i_malysh','zdorovoe_pitanie','vitaminy-i-obmen-veshchestv',
                 'preparaty-dlya-lecheniya-zabolevanij-kozhi', 'sredstva-ot-boli-vospaleniya-temperatury','ginekologicheskie-preparaty', 
                 'preparaty-dlya-kostno-myshechnoj-sistemy','preparaty-dlya-mochepolovoj-sistemy','preparaty-dlya-zreniya-i-sluha','preparaty-pri-allergii',
                 'preparaty-dlya-lecheniya-ehndokrinnoj-sistemy','preparaty-pri-zabolevaniyah-krovi', 'preparaty-dlya-lecheniya-infekcij',
                 'onkologiya-i-immunologiya','sredstva-ot-varikoza','preparaty-pri-zabolevaniyah-legkih','preparaty-dlya-borby-s-vrednymi-privychkami','preparaty-ot-parazitov',
-                'bazovaya-fitoterapiya','gomeopaticheskie-sredstva','vakciny-i-syvorotki'] 
-
-podcatolog_name_2 = ['mass-market','lechebnaya-i-selektivnaya-kosmetika'] 
-
-podcatolog_name_3 = ['predmety-detskoj-gigieny','predmety-uhoda-dlya-detej','tovary-dlya-beremennyh-i-kormyashchih','detskoe-pitanie','detskie-igrushki']
-
-podcatolog_name_4 = ['produkty-pitaniya-obshchego-naznacheniya','funkcionalnoe-pitanie','sportivnoe-pitanie']
-
-podcatolog_name_5 = ['medicinskaya-tehnika','perevyazochnye-sredstva','diagnosticheskie-test-sistemy','sredstva-uhoda-za-bolnymi',
-                     'izdeliya-medicinskie','funkcionalnoe-bele','ortopedicheskie-predmety','aptechki']
-
-podcatolog_name_6 = ['sredstva-dlya-uhoda-za-linzami-i-ochkami','linzy-kontaktnye','okklyudery','lupy','opravy','aksessuary-dlya-optiki','ochki']
-
-podcatolog_name_7 = ['dermakosmetika-uhod-za-volosami','chuvstvitelnaya-kozha-i-allergiya','dermakosmetika-antivozrastnoj-uhod',
-                     'dermakosmetika-zashchita-ot-solnca','dermakosmetika-problemnaya-kozha','vypadenie-i-perhot',
-                     'dermakosmetika-uvlazhnenie-i-pitanie', 'dermakosmetika-atopiya','dermakosmetika-uhod-za-telom','dermakosmetika-ochishchenie',
-                     'dermakosmetika-uhod-za-licom','dermakosmetika-dlya-detej','dermakosmetika-dlya-muzhchin','solnce']
-
-podcatolog_list = [podcatolog_name_1,podcatolog_name_2,podcatolog_name_3,podcatolog_name_4,podcatolog_name_5,podcatolog_name_6,podcatolog_name_7] 
+                'bazovaya-fitoterapiya','gomeopaticheskie-sredstva','vakciny-i-syvorotki', 'mass-market','lechebnaya-i-selektivnaya-kosmetika', 'predmety-detskoj-gigieny','predmety-uhoda-dlya-detej',
+                'tovary-dlya-beremennyh-i-kormyashchih','detskoe-pitanie','detskie-igrushki', 'produkty-pitaniya-obshchego-naznacheniya','funkcionalnoe-pitanie','sportivnoe-pitanie', 
+                'medicinskaya-tehnika','perevyazochnye-sredstva','diagnosticheskie-test-sistemy','sredstva-uhoda-za-bolnymi',
+                'izdeliya-medicinskie','funkcionalnoe-bele','ortopedicheskie-predmety','aptechki',
+                'sredstva-dlya-uhoda-za-linzami-i-ochkami','linzy-kontaktnye','okklyudery','lupy','opravy','aksessuary-dlya-optiki','ochki',
+                'dermakosmetika-uhod-za-volosami','chuvstvitelnaya-kozha-i-allergiya','dermakosmetika-antivozrastnoj-uhod',
+                'dermakosmetika-zashchita-ot-solnca','dermakosmetika-problemnaya-kozha','vypadenie-i-perhot',
+                'dermakosmetika-uvlazhnenie-i-pitanie', 'dermakosmetika-atopiya','dermakosmetika-uhod-za-telom','dermakosmetika-ochishchenie',
+                'dermakosmetika-uhod-za-licom','dermakosmetika-dlya-detej','dermakosmetika-dlya-muzhchin','solnce']
 
 catalog_name_cnt = 0 
-podcatalog_name_cnt = 0
-podcatolog_list_cnt = 0 
 page_num = 1 
 
-new_url = f"{url}/{podcatolog_list[podcatolog_list_cnt]}?page={page_num}"
+new_url = f"{url}/{catalog_name[catalog_name_cnt]}/?page={page_num}"
 
 driver = webdriver.Chrome()
 
@@ -74,7 +59,8 @@ while(availability_of_the_next_page):
     status = soup.find("svg", class_="sc-adb9b8f4-0 hKNQCg undefined app-icon-sprite__chevron_right-icon")
     if status != None:
         page_num += 1
-        new_url = f"{url}/{catalog_name[0]}/?page=1={page_num}"
+        catalog_name_cnt += 1
+        new_url = f"{url}/{catalog_name[catalog_name_cnt]}/?page={page_num}"
     else:
         availability_of_the_next_page = False
 
